8f6fb46eff48a9a78c4c657f0acf2f2f0e5406a1
TvChkr <bens640@gmail.com>
2020-11-12 12:49:06 -0500

    Fixed requirements.txt

--- TvChkr/settings.py
+++ TvChkr/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('TVCHKR_SECRET_KEY')
+SECRET_KEY = '$f&r2n^x+_my_n-^g&_k-2vo@(^o@zr)l8^cdc64povq^c(nd)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
