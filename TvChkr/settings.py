e54ca01c2e16de47da07a9ab1800f94492815778
TvChkr <bens640@gmail.com>
2020-11-12 13:12:38 -0500

    Fixed settings.py

--- TvChkr/settings.py
+++ TvChkr/settings.py
@@ -19,12 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$f&r2n^x+_my_n-^g&_k-2vo@(^o@zr)l8^cdc64povq^c(nd)'
-
+# SECRET_KEY = '$f&r2n^x+_my_n-^g&_k-2vo@(^o@zr)l8^cdc64povq^c(nd)'
+SECRET_KEY = os.environ.get('TVCHKR_SECRET_KEY_PROD')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tvchkr.herokuapp.com']
 
 # Application definition
 
